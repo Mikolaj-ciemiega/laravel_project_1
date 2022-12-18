1d97586ea1d917dd2fc1fef720dd69bb6bf89b4d
Mikolaj-ciemiega <mc305352@student.polsl.pl>
2022-12-18 19:34:22 +0100

    fixed api

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeopleController;
 
 
-//Route::get('peoples', [PeopleController::class, 'index']);
-//Route::get('peoples/{id}', [PeopleController::class, 'show']);
-//Route::post('peoples', [PeopleController::class, 'create']);
-//Route::get('peoples/{id}', [PeopleController::class, 'update']);
-//Route::get('peoples/{id}', [PeopleController::class, 'destroy']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+
+});
 
 Route::apiResource('peoples', PeopleController::class);
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-   // return $request->user();
-
-//});
