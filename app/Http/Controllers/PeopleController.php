f1d3e91ab704259423de0b31cef446e8bdc3594f
Mikolaj-ciemiega <mc305352@student.polsl.pl>
2022-12-18 17:45:15 +0100

    create people controller

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\People;
+use Illuminate\Http\JsonResponse;
+use App\Requests\PeoplePostRequest;
+use Illuminate\Support\Facades\DB;
+
+class PeopleController extends Controller{
+
+    public function index(): JsonResponse
+    {
+        return response()->json(People::all(), 200);
+    }
+
+    public function create(Request $request): JsonResponse
+    {  
+            $people = new People;
+            $people->category_name = $request->category_name;
+            $people->save();
+            return response()->json(null, 201);
+    }
+
+    public function show($id): JsonResponse
+    {
+       $people = DB::table('peoples')->where('id', $id)->first();
+       return response()->json($people);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $data = array();
+        $data['category_name'] = $request->category_name;
+        $user = DB::table('categories')->where('id', $id)->update($data);
+        return response()->json(null, 200);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        DB::table('categories')->where('id', $id)->delete();
+        return response()->json(null, 200);
+    }
+}
